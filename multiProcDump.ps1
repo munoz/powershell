b40a3ab3fdc7444aabec4c334cd4ffdff7eb6679
Pedro Muñoz <munoz@users.noreply.github.com>
2017-11-13 22:28:59 -0800

    Add files via upload
    Used to run multiple procdump64.exe jobs in parallel

---
+++ multiProcDump.ps1
@@ -0,0 +1,15 @@
+﻿#This creates a list of
+$ids.Clear()
+$ids = Get-Process -Name iexplore | Select -ExpandProperty Id
+
+workflow processDump {
+    param ([string[]]$ids)
+
+    foreach -parallel ($id in $ids) {
+        Invoke-Expression 'C:\Utils\procdump64.exe -ma $id -accepteula "C:\Utils\Dumps\$($id).dmp"' 
+    }
+    
+}
+
+processDump -ids $ids
+	
